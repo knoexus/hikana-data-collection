36605d37ed1cadfa48fc4801a54cb0d0faf9915f
knoexus <knoexus@yahoo.com>
2023-09-29 21:49:18 +0700

    add kashudo's most common words parsing

---
+++ src/dto/kashudo.py
@@ -0,0 +1,9 @@
+class KashudoWord:
+    def __init__(self, kanji: str | None, kana: str | None, occurence_level: int, jlpt_level: int) -> None:
+        self.kanji = kanji
+        self.kana = kana
+        self.occurence_level = occurence_level
+        self.jlpt_level = jlpt_level
+    
+    def __str__(self) -> str:
+        return f'Kanji: {self.kanji}; kana: {self.kana}, occurence level: {self.occurence_level}, jlpt level: {self.jlpt_level}'
---
+++ src/kashudo.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+from dto.kashudo import KashudoWord
+
+
+class WordFinder:
+    def get_string_between_substrings(self, string: str, substring1: str = '', substring2: str = '') -> str | None:
+        try:
+            return string[string.find(substring1)+len(substring1):string.rfind(substring2)]
+        except BaseException:
+            return None
+
+
+class ExpandedRequests:
+    def __init__(self) -> None:
+        self.headers = { 'User-Agent': 'Mozilla/5.0' }
+        self.soup_mode = 'html.parser'
+
+    def get(self, *args, **kwargs) -> requests.Response:
+        return requests.get(*args, **kwargs, headers=self.headers)
+    
+    def get_soup_content(self, *args, **kwargs) -> BeautifulSoup:
+        html = self.get(*args, **kwargs).content
+        return BeautifulSoup(html, self.soup_mode)
+
+
+class KashudoCrawler:
+    def __init__(self) -> None:
+        self.main_page_url = 'https://www.kanshudo.com/collections/vocab_usefulness2021'
+        self.words_page_template_url = self.main_page_url + '/UFN2021-'
+        self.words_page_template_offset = 100
+        self.erequests = ExpandedRequests()
+        self.word_finder = WordFinder()
+
+    def get_level_word_counts(self) -> dict:
+        soup = self.erequests.get_soup_content(self.main_page_url)
+        title_elements = soup.select('.infopanel .title_h4')
+        titles = [title_element.get_text() for title_element in title_elements]
+        level_counts = [int(self.word_finder.get_string_between_substrings(title, '(', 'words)')) for title in titles]
+        return dict((i + 1, value) for i, value in enumerate(level_counts))
+
+    def get_words_on_all_pages(self) -> [KashudoWord]:
+        level_words_counts = self.get_level_word_counts()
+
+        words = []
+        for level, words_available in level_words_counts.items():
+            current_page = 0
+            while current_page * self.words_page_template_offset < words_available:
+                words_on_one_page = self.get_words_on_one_page(level, current_page)
+                words.extend(words_on_one_page)
+                current_page += 1
+                print(level, current_page)
+        return words
+
+    def get_words_on_one_page(self, level: int, page: int) -> [KashudoWord]:
+        url_postfix = f'{level}-{self.words_page_template_offset * page + 1}'
+        words_page_url = self.words_page_template_url + url_postfix
+        soup = self.erequests.get_soup_content(words_page_url)
+        
+        words = []
+        jukugorows = soup.select('.jukugorow')
+        for jukugorow in jukugorows:
+            jukugo_element = jukugorow.select_one('.jukugo')
+            kanji_element = jukugo_element.select_one('.f_kanji')
+            furigana_element = jukugo_element.select_one('.furigana')
+            if kanji_element is None and furigana_element is None:
+                kanji = None
+                kana = jukugo_element.select_one('a').get_text()
+            else:
+                kanji = kanji_element.get_text()
+                kana = furigana_element.get_text()
+
+            jlpt_element = jukugorow.select_one('.jlpt_container > span')
+            jlpt_level = jlpt_element.get('class')[0][-1] if jlpt_element is not None else None
+
+            model = KashudoWord(kanji=kanji, kana=kana, occurence_level=level, jlpt_level=jlpt_level)
+            words.append(model)
+        return words
+
+
+kc = KashudoCrawler()
+res = kc.get_words_on_all_pages()
